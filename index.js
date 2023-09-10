3b865d11f079c5bf14a0db9cd6802f541ccee5e3
Mathew <mathewlmcdowell@gmail.com>
2023-09-10 11:18:32 -0500

    solution

--- index.js
+++ index.js
@@ -1,51 +1,39 @@
-// import the pets array from data.js
 const pets = require('./data');
-
-// init express app
 const express = require('express');
 const app = express();
 
 const PORT = 8080;
 
-// GET - / - returns homepage
-app.get('/', (req, res) => {
-    // serve up the public folder as static index.html file
 
+app.get('/', (req, res) => {
+
+    res.sendFile(__dirname + '/public/index.html');
 });
 
-// hello world route
 app.get('/api', (req, res) => {
     res.send('Hello World!');
 });
 
-// get all pets from the database
-app.get('/api/v1/pets', (req, res) => {
-    // send the pets array as a response
 
+app.get('/api/v1/pets', (req, res) => {
+
+    res.send(pets);
 });
 
 // get pet by owner with query string
 app.get('/api/v1/pets/owner', (req, res) => {
-    // get the owner from the request
 
-
-    // find the pet in the pets array
-    const pet = pets.find(pet => pet.owner === owner);
-
-    // send the pet as a response
-
+    const { owner } = req.query;
+    const pet = pets.filter(pet => pet.owner.toLowerCase() === owner.toLowerCase());
+    res.send(pet);
 });
 
 // get pet by name
 app.get('/api/v1/pets/:name', (req, res) => {
-    // get the name from the request
 
-
-    // find the pet in the pets array
-    const pet = pets.find(pet => pet.name === name);
-
-    // send the pet as a response
-
+    const name = req.params.name;
+    const pet = pets.find(pet => pet.name.toLowerCase() === name.toLowerCase());
+    res.send(pet);
 });
 
 app.listen(PORT, () => {
